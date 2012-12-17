a229a95cf9b23db85957408594fbb7262b8a40fc
Klemens Schindler <klemensschindler@gmail.com>
2012-12-17 21:34:29 +0100

    Moved parameters which are always passed to class members.

--- ref/java/Questionnaire.java
+++ ref/java/Questionnaire.java
@@ -5,7 +5,10 @@ import java.awt.event.*;
 import java.util.*;
 
 public class Questionnaire {
-  public static void updateQuestionnaire(final Map<String, Component> questionnaireWidgets, final JFrame frame) {
+  final JFrame frame = new JFrame("Questionnaire");
+  final Map<String, Component> questionnaireWidgets = new HashMap<String, Component>();
+
+  public static void updateQuestionnaire() {
     questionnaireWidgets.get("block").setVisible(((JCheckBox)questionnaireWidgets.get("hasSoldHouse")).isSelected() ? true : false);
     if (
         !((JTextField)questionnaireWidgets.get("houseSoldPrice")).getText().trim().equals("") && 
@@ -19,7 +22,7 @@ public class Questionnaire {
     frame.pack();
   }
 
-  public static Component createBooleanQuestion(String questionName, String question, final Map<String, Component> questionnaireWidgets, final JFrame frame) {
+  public static Component createBooleanQuestion(String questionName, String question) {
     JPanel panel = new JPanel();
     panel.add(new JLabel(question));
     JCheckBox cb = new JCheckBox();
@@ -27,14 +30,14 @@ public class Questionnaire {
     cb.addItemListener(
         new ItemListener() {
           public void itemStateChanged(ItemEvent e) {
-            updateQuestionnaire(questionnaireWidgets, frame);
+            updateQuestionnaire();
           }
         });
     panel.add(cb);
     return panel;
   }
 
-  public static Component createStringQuestion(String questionName, String question, final Map<String, Component> questionnaireWidgets, final JFrame frame) {
+  public static Component createStringQuestion(String questionName, String question) {
     JPanel panel = new JPanel();
     panel.add(new JLabel(question));
     JTextField tf = new JTextField("         ");
@@ -42,20 +45,20 @@ public class Questionnaire {
     tf.getDocument().addDocumentListener(
         new DocumentListener() {
           public void changedUpdate(DocumentEvent documentEvent) {
-            updateQuestionnaire(questionnaireWidgets, frame);
+            updateQuestionnaire();
           }
           public void insertUpdate(DocumentEvent documentEvent) {
-            updateQuestionnaire(questionnaireWidgets, frame);
+            updateQuestionnaire();
           }
           public void removeUpdate(DocumentEvent documentEvent) {
-            updateQuestionnaire(questionnaireWidgets, frame);
+            updateQuestionnaire();
           }
         });
     panel.add(tf);
     return panel;
   }
 
-  public static Component createCalculatedValue(String questionName, String question, final Map<String, Component> questionnaireWidgets, final JFrame frame) {
+  public static Component createCalculatedValue(String questionName, String question) {
     JPanel panel = new JPanel();
     panel.add(new JLabel(question));
     JLabel value = new JLabel();
@@ -65,25 +68,22 @@ public class Questionnaire {
   }
 
 public static void main(String[] args) {
-  final JFrame frame = new JFrame("Questionnaire");
   frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.PAGE_AXIS));
 
-  final Map<String, Component> questionnaireWidgets = new HashMap<String, Component>();
-
   final JPanel block = new JPanel();
   questionnaireWidgets.put("block", block);
-  frame.getContentPane().add(createBooleanQuestion("hasSoldHouse", "Did you sell a house in 2010?", questionnaireWidgets, frame));
+  frame.getContentPane().add(createBooleanQuestion("hasSoldHouse", "Did you sell a house in 2010?"));
   block.setLayout(new BoxLayout(block, BoxLayout.PAGE_AXIS));
   block.add(createStringQuestion("houseSoldPrice", "Price the house was sold for:", questionnaireWidgets, frame));
   block.add(createStringQuestion("privateDebts", "Private debts for the sold house:", questionnaireWidgets, frame));
   block.add(createCalculatedValue("valueResidue", "Value residue:", questionnaireWidgets, frame));
   frame.getContentPane().add(block);
 
-  frame.getContentPane().add(createBooleanQuestion("hasBoughtHouse", "Did you buy a house in 2010?", questionnaireWidgets, frame));
-  frame.getContentPane().add(createBooleanQuestion("hasEnteredLoan", "Did you enter a loan for maintenance/reconstruction?", questionnaireWidgets, frame));
+  frame.getContentPane().add(createBooleanQuestion("hasBoughtHouse", "Did you buy a house in 2010?"));
+  frame.getContentPane().add(createBooleanQuestion("hasEnteredLoan", "Did you enter a loan for maintenance/reconstruction?"));
 
-  updateQuestionnaire(questionnaireWidgets, frame);
+  updateQuestionnaire();
 
   frame.pack();
   frame.setVisible(true);
